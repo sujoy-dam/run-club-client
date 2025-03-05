9d2ae5ccdb463ea5e2d6111e67555224f60f93ee
sujoy-dam <sujooy01@gmail.com>
2025-03-05 14:54:35 +0600

    added icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,8 +4,11 @@
 // import AddMarathon from './AddMarathon';
 // import MyMarathonsList from './MyMarathonsList';
 
-import { Link, NavLink, Outlet } from "react-router-dom";
+import {NavLink, Outlet } from "react-router-dom";
 import "./Dashboard.css";
+import { FaAddressBook } from "react-icons/fa6";
+import { FaThList } from "react-icons/fa";
+import { RiFileListFill } from "react-icons/ri";
 
 // import MyApplyList from './MyApplyList';
 const Dashboard = () => {
@@ -13,27 +16,30 @@ const Dashboard = () => {
         <div className="min-h-screen flex">
       {/* Sidebar */}
       <aside className="w-64 bg-gray-800 text-white flex flex-col">
-        <div className="p-4 text-lg font-bold">Dashboard</div>
+        <div className="p-4 text-2xl font-bold space-x-4">Dashboard</div>
         <nav className="flex-1 px-4">
           <ul className="space-y-4">
-            <li className="my-2">
-              <NavLink to="/dashboard/add-marathon" className="block py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
-                ADD Marathon
+            <li className="my-2 font-bold text-lg">
+              <NavLink to="/dashboard/add-marathon" className="flex items-center gap-2 py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
+              <FaAddressBook className="text-xl"/>
+                Add Marathon
               </NavLink>
             </li>
-            <li className="my-2">
-              <NavLink to="my-marathons-list" className="block py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
+            <li className="my-2 font-bold text-lg">
+              <NavLink to="my-marathons-list" className="flex items-center gap-2 py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
+              <FaThList className="text-xl"/>
                My Marathons List
               </NavLink>
             </li>
-            <li className="my-2">
-              <NavLink to="my-apply-list" className="block py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
+            <li className="my-2 font-bold text-lg">
+              <NavLink to="my-apply-list" className="flex items-center gap-2 py-2 px-3 rounded hover:outline-2 hover:outline-pink-500 hover:outline">
+              <RiFileListFill className="text-2xl"/>
                 My Apply List
               </NavLink>
             </li>
           </ul>
         </nav>
-        <div className="p-4">Footer</div>
+        {/* <div className="p-4">Footer</div> */}
       </aside>
 
       {/* Main Content */}
